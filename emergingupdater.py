be8719ceea86a0ec6390d97fdf72d0ee75fe31dd
yeknu <equipo_seguridad@gruposothis.com>
2018-04-19 18:44:13 +0200

    script push

---
+++ emergingupdater.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import os
+import urllib2
+
+md5file='lastmd5'
+rulesdirectory='./rules.tar.gz'
+rulesurl='https://rules.emergingthreats.net/open-nogpl/suricata-4.0/emerging.rules.tar.gz'
+
+#Actualizacion de reglas
+def deploynewrules():
+	os.system("wget " + rulesurl + " -o wget.output -O" + rulesdirectory)
+	os.system("tar -xzf " + rulesdirectory)
+
+#Comprobar si ha cambiado el md5
+def md5change():
+	re = urllib2.urlopen(rulesurl+".md5")
+	newmd5 = re.read()
+
+	oldmd5file = open(md5file,"r")
+	oldmd5 = oldmd5file.read()
+	oldmd5file.close()
+
+	if newmd5!=oldmd5 :
+		md5 = open (md5file,"w")
+		md5.write(newmd5)
+		md5.close()
+		return 1
+
+	else:
+		return 0
+
+def main():     
+	if os.path.exists(md5file):
+		if md5change():
+			deploynewrules()
+	else:
+		re = urllib2.urlopen(rulesurl+".md5")
+		newmd5 = re.read()
+		md5 = open (md5file,"w")
+		md5.write(newmd5)
+		md5.close()
+		deploynewrules()
+
+
+if __name__ == '__main__':
+	main()
